806d0d8f44e063249464339ec2f3ce553d1dcccc
PassosLucasB <PassosL@outlook.com>
2022-06-09 16:22:50 -0300

    add 28

--- exercicios/28.php
+++ exercicios/28.php
@@ -3,29 +3,29 @@
     $dados = array(
         "1" => array(
             "nome" => "Almir",
-            "idade" => "18".
+            "idade" => "18",
             "CEP" => "00000-001",
-            "emails" => array(
-                '1' => "almir@mailhot.com",
-                '2' => "almir@mailg.com"
-            )
             ),
-        "2"    
-    )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        "2" => array(
+            "nome" => "Bino",
+            "idade" => "18",
+            "CEP" => "00000-002",
+            ),
+        "3" => array(
+            "nome" => "Claudio",
+            "idade" => "18",
+            "CEP" => "00000-003",
+            ),  
+        );
+        foreach ($dados as $x){
+             echo "<br> Chave: $key";
+             foreach ($x as $subkey ){
+                 echo "<br>";
+                 var_dump($subkey);
+                    echo $subkey['nome'];
+                    echo $subkey['idade'];
+                    echo $subkey['CEP'];
+             }
+        }
 
 ?>
