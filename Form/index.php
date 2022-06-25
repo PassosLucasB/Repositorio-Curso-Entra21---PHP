9b2238e5b0ea9741c4a8c00fd94a2a80fea9cdfc
Lucas Bernardelli dos Passos <PassosL@outlook.com>
2022-06-25 20:48:37 -0300

    add_css

--- Form/index.php
+++ Form/index.php
@@ -6,35 +6,43 @@
 
 <head>
     <meta charset="UTF-8">
-    <meta name="Lucas Benardelli dos Passos" content="">
+    <meta name="Lucas Bernardelli dos Passos" content="">
     <meta name="Formulário em PHP" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/tools/style.css">
     <title>Formulário de Dados</title>
 </head>
 <body>
-    <section class="content">
+<containter class="header">
+
+<h2 class="item-header">Site de treinamento de programação em PHP - Programa Entra21</h2>
+<div class="item-21"><img src="https://user-images.githubusercontent.com/46844031/166827406-389e0e71-81b6-461b-a725-58f059e1a355.png" alt="imagem do entra21"></div>
+<a href="https://www.entra21.com.br/">Link</a>
+</containter>
+<container class="content">
         <div class="contato">
-
-   
         <h3>FORMULÁRIO DE DADOS</h3>
-
-            <form action="script.php" method="post">
-    
+            <form action="/tools/script.php" method="post">
                 <p>Insira o Nome: <input class="field" type="text" name="nome" /></p>
                 <p>Insira a Idade: <input class="field" type="text" name="idade" /></p>
                 <p>Insira o CEP: <input class="field" type="text" name="cep" /></p>
-                <p><input type="submit" value="Enviar dados"/></p>
+                <p><input class="field" type="submit" value="Enviar dados"/></p>
             </form>
-
         </div>
-        
-    </section>
+        <div class="quadro">
+            <h4 class="item-quadro">Quadro de Referências</h4>
+            <a href="https://www.w3schools.com/default.asp">w3schools</a>
+            <a href="https://www.php.net/manual/pt_BR/">php.net</a>
+            <a href="https://www.linkedin.com/in/lucas-bernardelli-dos-passos-a74242174">Linkedin</a>
+            <a href="https://github.com/PassosLucasB">GitHub</a>
+        </div>
+</container>
+<container class="support">
+<h5>Desenvolvido por: Lucas Bernardelli dos Passos</h5>
 
-    
-    
 
 
 
+</container>
 </body>
 </html>
--- Form/tools/script.php
+++ Form/tools/script.php
@@ -1,9 +1,19 @@
 <?php
 
+if(isset($_POST['name']) && !empty($_POST['name'])){
+
 $nome = addslashes($_POST['name']);
 $idade = addslashes($_POST['idade']);
-$cep = addslashes($_POST['cep'])
+$cep = addslashes($_POST['cep']);
 
+$body = "Nome: ".$nome. "\r\n".
+        "Idade: ".$idade. "\r\n".
+        "Cep: ".$cep. "\r\n";
+
+
+
+
+}
 /*
 - Utilize arquivo separado (modularizado) para tratar as alterações.
 - Inclua um botão excluir um item da sua array.
